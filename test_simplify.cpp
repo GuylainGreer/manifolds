86815dcdbdb366b47bf3dbed8dca77ddc8798130
GuylainGreer <guylaingreer@gmail.com>
2014-06-19 13:26:52 -0400

    Simplify -> Simplification

--- test_simplify.cpp
+++ test_simplify.cpp
@@ -11,19 +11,19 @@ BOOST_AUTO_TEST_CASE(simplify_test)
   using namespace manifolds;
   typedef Addition<Sin,Cos> Type;
   static_assert(std::is_same<
-		Simplify<Type>::type,
+		Simplification<Type>::type,
 		Type>::value, "Failed to not simplify!");
 
   static_assert(std::is_same<
-		Simplify<Addition<Addition<Sin,Cos>, Tan>>::type,
+		Simplification<Addition<Addition<Sin,Cos>, Tan>>::type,
 		Addition<Sin, Cos, Tan>>::value,
 		"Failed to collapse nested additions!");
   Addition<Addition<Sin,Cos>, Tan> t;
-  auto t2 = Simplify<decltype(t)>::Combine(t);
+  auto t2 = Simplification<decltype(t)>::Combine(t);
   static_assert(std::is_same<
 		decltype(t2),
 		Addition<Sin,Cos,Tan>>::value,
-		"Simplify<>::Combine failed");
+		"Simplification<>::Combine failed");
 
   /*  static_assert(std::is_same<
 		decltype(Sin()+Sin()),
