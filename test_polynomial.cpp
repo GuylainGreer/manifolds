9c09d9e9296233a4d2045b2138fd39da099286ec
GuylainGreer <guylaingreer@gmail.com>
2014-06-19 13:26:21 -0400

    Added tests for simplification of polynomials

--- test_polynomial.cpp
+++ test_polynomial.cpp
@@ -1,5 +1,6 @@
 #include "polynomial.hh"
 #include "full_function_defs.hh"
+#include "operators.hh"
 #include <boost/test/unit_test.hpp>
 #include <ratio>
 
@@ -16,10 +17,25 @@ BOOST_AUTO_TEST_CASE(polynomial_test)
   BOOST_CHECK_EQUAL(p(2), 2);
 
   auto p2 = GetPolynomial<double>(2,5,6);
-  auto p3 = Simplify<Addition<decltype(p),
-			      decltype(p2)>>::Combine(p,p2);
+  auto p3 = p + p2;
+  static_assert(std::is_same<
+		decltype(p3),
+		Polynomial<double,double,
+		std::integral_constant<int,3>>>::value,
+		"Failed to simplify adding two polynomials!");
   BOOST_CHECK_EQUAL(p3(0), 3);
   BOOST_CHECK_EQUAL(p3(1), 14.5);
   BOOST_CHECK_EQUAL(p3(-1), 3.5);
+
+  auto p4 = p * p2;
+  static_assert(std::is_same<
+		decltype(p4),
+		Polynomial<double,double,
+		std::integral_constant<int,4>>>::value,
+		"Failed to simplify multiplying"
+		" two polynomials!");
+  BOOST_CHECK_EQUAL(p4(0), p(0)*p2(0));
+  BOOST_CHECK_EQUAL(p4(1), p(1)*p2(1));
+  BOOST_CHECK_EQUAL(p4(2), p(2)*p2(2));
 }
 
