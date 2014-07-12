e61134b300ea2ce53ae66cb13f3c3c546398e7d8
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 17:02:33 -0400

    Changed t,x,y,z to x,y,z,t and added non-abelian variable tag for matrices

--- variables.hh
+++ variables.hh
@@ -7,39 +7,55 @@
 
 namespace manifolds {
 
-  template <int N>
+  template <int N, bool abelian>
   struct VariableImpl : MultiFunction
   {
+  private:
+    template<class>
+    struct not_tuple:std::true_type{};
+    template <class...As>
+    struct not_tuple<std::tuple<As...>>:std::false_type{};
+  public:
     static const bool stateless = true;
-    template <class ... Args>
-    auto&& operator()(Args && ... args) const
+    static const bool abelian_arithmetic = abelian;
+    template <class Arg, class ... Args,
+	      class = typename std::enable_if<
+		!is_function<Arg>::value>::type>
+    auto operator()(Arg arg, Args ... args) const
     {
-      static_assert(N < sizeof...(Args),
+      static_assert(N < sizeof...(Args)+1,
 		    "Not enough arguments to extract Nth");
-      typedef decltype(std::tie(args...)) tuple_type;
-      typedef typename std::tuple_element<N, tuple_type>::type tuple_elem;
-      return std::forward<tuple_elem>(std::get<N>(tuple_type(args...)));
+      typedef decltype(std::tie(arg,args...)) tuple_type;
+      return std::get<N>(tuple_type(arg,args...));
     }
   };
 
-  template <int N>
-  struct Variable : VariableImpl<N>,
-    FunctionCommon<Variable<N>>
+  template <int N, bool abelian = true>
+  struct Variable : VariableImpl<N,abelian>,
+    FunctionCommon<Variable<N,abelian>>
   {
     using FunctionCommon<Variable>::operator();
-    template <class Arg, class ... Args, class =
-	      typename std::enable_if<
-                  !is_function<Arg>::value>::type>
-      auto operator()(Arg arg, Args ... args) const
-    {
-      return VariableImpl<N>::operator()(arg,args...);
-    }
+    using VariableImpl<N, abelian>::operator();
   };
 
-  static const Variable<0> t;
-  static const Variable<1> x;
-  static const Variable<2> y;
-  static const Variable<3> z;
+#define VARIABLE(i, abelian, name)				\
+  static const Variable<i,abelian> name;			\
+  inline std::ostream & operator<<(std::ostream&s,		\
+				   Variable<i,abelian>)		\
+  {return s << #name;}
+
+  VARIABLE(0,true,x)
+  VARIABLE(1,true,y)
+  VARIABLE(2,true,z)
+  VARIABLE(3,true,t)
+
+  //WARNING! The following variables are non-abelian,
+  //meaning that A*B != B*A, use them for matrices
+  //(for example)
+  VARIABLE(0,false,X)
+  VARIABLE(1,false,Y)
+  VARIABLE(2,false,Z)
+  VARIABLE(3,false,T)
 
 }
 
