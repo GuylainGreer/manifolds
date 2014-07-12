87c488b8550ce72b72c823689b612e84cff180a3
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 17:00:50 -0400

    Added insert_element helper function

--- tuple_util.hh
+++ tuple_util.hh
@@ -30,6 +30,30 @@ namespace manifolds {
     return remove_element_helper(t, befores(), afters());
   }
 
+  template <int index, class Tuple, class E,
+	    std::size_t ... befores,
+	    std::size_t ... afters>
+  auto insert_element_helper(const Tuple & t,
+			     const E & e,
+			     std::integer_sequence<
+			     std::size_t, befores...>,
+			     std::integer_sequence<
+			     std::size_t, afters...>)
+  {
+    auto before = std::make_tuple(std::get<befores>(t)...);
+    auto middle = std::make_tuple(e);
+    auto after  = std::make_tuple(std::get<afters+index>(t)...);
+    return std::tuple_cat(before, middle, after);
+  }
+
+  template <int index, class Tuple, class E>
+  auto insert_element(const Tuple & t, const E & e)
+  {
+    return insert_element_helper<index>
+      (t, e, std::make_index_sequence<index>(),
+       std::make_index_sequence<
+       std::tuple_size<Tuple>::value - index>());
+  }
 }
 
 #endif
