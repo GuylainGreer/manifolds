2df5c795dd7c0961cc589d7e711100e974effdb1
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 16:56:08 -0400

    changed variable indices

--- test_unaryminus.cpp
+++ test_unaryminus.cpp
@@ -8,6 +8,6 @@ BOOST_AUTO_TEST_CASE(unary_minus_test)
   using namespace manifolds;
   UnaryMinus<decltype(x)> a(x);
 
-  BOOST_CHECK_EQUAL(a(1,2,3,4), -2);
-  BOOST_CHECK_EQUAL(a(1,2,4,8,16), -2);
+  BOOST_CHECK_EQUAL(a(1,2,3,4), -1);
+  BOOST_CHECK_EQUAL(a(1,2,4,8,16), -1);
 }
