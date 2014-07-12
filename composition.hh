41d271c0775262d61b5212594d2fdfeaea9a058a
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 16:26:22 -0400

    Little tweaks and new simplification code

--- composition.hh
+++ composition.hh
@@ -3,6 +3,9 @@
 
 #include "is_function.hh"
 #include "function.hh"
+#include "equals.hh"
+#include "streaming.hh"
+#include "simplify_variadic.hh"
 
 namespace manifolds {
 
@@ -12,30 +15,46 @@ struct Composition : MultiFunction
   static const bool stateless =
     and_<is_stateless<Functions>...>::value;
 
+  static_assert(sizeof...(Functions)>0,
+		"Cannot have empty composition!");
   Composition(){}
   Composition(const Functions & ... functions):
     functions(functions...){}
+  Composition(const std::tuple<Functions...> & f):
+    functions(f){}
 
   template <class Arg, class ... Args,
 	    class = typename std::enable_if<
 	      !is_function<Arg>::value
 	      >::type>
-  auto operator()(Arg arg, Args && ... args) const
+  auto operator()(Arg arg, Args ... args) const
   {
     static const int last = sizeof...(Functions)-1;
-    return eval<last-1>(std::get<last>(functions)(arg, args...));
+    return eval(std::get<last>(functions)(arg, args...),
+		int_<last-1>());
+  }
+
+  auto operator()() const
+  {
+    static const int last = sizeof...(Functions)-1;
+    return eval(std::get<last>(functions)(),
+		int_<last-1>());
+  }
+
+  template <class Arg>
+  auto eval(Arg arg, int_<0>) const
+  {
+    typedef decltype(std::get<0>(functions)(arg)) result;
+    return std::forward<result>(std::get<0>(functions)(arg));
   }
 
   template <int N, class Arg>
-  auto eval(Arg && arg) const
+  auto eval(Arg arg, int_<N>) const
   {
     typedef decltype(std::get<N>(functions)(arg)) result;
-    static const int next_N = N == 0? 0 : N-1;
-    if(N == 0)
-      return std::forward<result>(std::get<N>(functions)(arg));
-    else
-      return eval<next_N>(std::forward<result>
-			  (std::get<N>(functions)(arg)));
+    return eval(std::forward<result>
+		(std::get<N>(functions)(arg)),
+		int_<N-1>());
   }
 
   template <class InnerFunc>
@@ -48,7 +67,7 @@ struct Composition : MultiFunction
   template <class InnerFunc, class = typename std::enable_if<
 			       is_function<InnerFunc>::value
 			       >::type>
-  auto operator()(InnerFunc && f) const
+  auto operator()(InnerFunc f) const
   {
     return Composition<
       Composition,
@@ -60,9 +79,77 @@ struct Composition : MultiFunction
     return functions;
   }
 
+  void stream(std::ostream & s,
+			int_<sizeof...(Functions)>)
+  {
+  }
+
+  template <int N>
+  void stream(std::ostream & s, int_<N>)
+  {
+    std::get<N>(functions).Print(s);
+    s << "(";
+    stream(s, int_<N+1>());
+    s << ")";
+  }
+
+  template <class T>
+  bool operator==(const T & t) const
+  {
+    return VariadicEqual(*this, t);
+  }
+
+  void Print(std::ostream & s)
+  {
+    stream(s, int_<0>());
+  }
 private:
   std::tuple<Functions...> functions;
 };
+
+  template <class> struct is_composition : std::false_type{};
+
+  template <class...Args>
+  struct is_composition<Composition<Args...>> :
+    std::true_type{};
+
+  template <class ... Args>
+  std::ostream & operator<<(std::ostream & s,
+			    Composition<Args...> c)
+  {
+    StreamVariadic("Composition", c, s);
+    return s;
+  }
+
+  template <class ... Functions>
+  struct Simplification<
+    Composition<Functions...>,
+    typename std::enable_if<(sizeof...(Functions)>2)>::type>
+  {
+    typedef decltype(SimplifyV<Composition>
+		     (std::declval<std::tuple<Functions...>>(),
+		      std::false_type{})) type;
+
+    static type Combine(Composition<Functions...> a)
+    {
+      return SimplifyV<Composition>(a.GetFunctions(),
+				    std::false_type{});
+    }
+  };
+
+  template <class F, class ... Functions>
+  struct Simplification<
+    Composition<F, Composition<Functions...>>>
+  {
+    typedef Composition<F, Functions...> type;
+
+    static type Combine(Composition<F, Composition<Functions...>> c)
+    {
+      auto t = c.GetFunctions();
+      return {std::tuple_cat(remove_element<1>(t),
+			     std::get<1>(t).GetFunctions())};
+    }
+  };
 
 }
 
