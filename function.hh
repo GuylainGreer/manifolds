51e6fb8c750d08bd2df6ba4b97570c814d85f904
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 16:34:00 -0400

    Added abelian metadata tag

--- function.hh
+++ function.hh
@@ -9,6 +9,9 @@ namespace manifolds {
   struct Function
   {
     static const bool stateless = false;
+    //Means you can change the order of
+    //Multiplications, ie A*B == B*A
+    static const bool abelian_arithmetic = true;
   };
 
   //Tag
@@ -24,12 +27,26 @@ namespace manifolds {
       bool, value> type;
   };
 
+  template <class T>
+  struct has_abelian_arithmetic
+  {
+    static const bool value = T::abelian_arithmetic;
+    typedef std::integral_constant<
+      bool, value> type;
+  };
+
   template <class Arg, class ... Args>
   struct and_ : std::conditional<
-    Arg::value, and_<Args...>, std::false_type>{};
+    Arg::value, and_<Args...>, std::false_type>::type{};
 
   template <class Arg>
   struct and_<Arg> : std::conditional<
-    Arg::value, std::true_type, std::false_type>{};
+    Arg::value, std::true_type, std::false_type>::type{};
+
+  template <int N>
+  using int_ = std::integral_constant<int, N>;
+
+  template <bool B>
+  using bool_ = std::integral_constant<bool, B>;
 }
 #endif
