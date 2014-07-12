10714ceaf5ca7538a8b20529983bf1cb17428164
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 16:46:03 -0400

    Changed variable indices

--- test_addition.cpp
+++ test_addition.cpp
@@ -8,6 +8,6 @@ BOOST_AUTO_TEST_CASE(addition_test)
   using namespace manifolds;
   Addition<decltype(x), decltype(z)> a(x,z);
 
-  BOOST_CHECK_EQUAL(a(1,2,3,4), 6);
-  BOOST_CHECK_EQUAL(a(1,2,4,8,16), 10);
+  BOOST_CHECK_EQUAL(a(1,2,3,4), 4);
+  BOOST_CHECK_EQUAL(a(1,2,4,8,16), 5);
 }
