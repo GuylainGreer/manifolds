0c280dc391b13a1be4fb4b06253d261a49b14418
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 16:49:28 -0400

    Changed variable indices

--- test_multiplication.cpp
+++ test_multiplication.cpp
@@ -8,6 +8,6 @@ BOOST_AUTO_TEST_CASE(multiplication_test)
   using namespace manifolds;
   Multiplication<decltype(x), decltype(z)> a(x,z);
 
-  BOOST_CHECK_EQUAL(a(1,2,3,4), 8);
-  BOOST_CHECK_EQUAL(a(1,2,4,8,16), 16);
+  BOOST_CHECK_EQUAL(a(1,2,3,4), 3);
+  BOOST_CHECK_EQUAL(a(1,2,4,8,16), 4);
 }
