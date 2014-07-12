0d3cee91443dcb6bd212ff5ef44905c64d4abc96
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 16:34:31 -0400

    Little tweaks and added new simplification code

--- multiplication.hh
+++ multiplication.hh
@@ -3,6 +3,9 @@
 
 #include "function.hh"
 #include "full_function_defs.hh"
+#include "equals.hh"
+#include "streaming.hh"
+#include "simplify_variadic.hh"
 
 namespace manifolds {
 
@@ -15,30 +18,40 @@ struct MultiplicationImpl : MultiFunction
   MultiplicationImpl(){}
   MultiplicationImpl(const Functions & ... functions):
     functions(functions...){}
+  MultiplicationImpl(const std::tuple<Functions...> & f):
+    functions(f){}
 
   template <class ... Args>
-  auto operator()(Args && ... args) const
+  auto operator()(Args ... args) const
   {
-    return eval<0>(std::forward<Args>(args)...);
+    return eval(int_<0>(), args...);
+  }
+
+  template <class ... Args>
+  auto eval(int_<sizeof...(Functions)-1>, Args ... args) const
+  {
+    return std::get<sizeof...(Functions)-1>
+      (functions)(args...);
   }
 
   template <int N, class ... Args>
-  auto eval(Args && ... args) const
+  auto eval(int_<N>, Args ... args) const
   {
     typedef decltype(std::get<N>(functions)(args...)) result;
-    static const int next_N =
-      N == sizeof...(Functions)-1?
-      sizeof...(Functions)-1 : N+1;
-    if(N == sizeof...(Functions) - 1)
-      return std::forward<result>(std::get<N>(functions)(args...));
-    else
-      return std::forward<result>(std::get<N>(functions)(args...)) *
-	eval<next_N>(std::forward<Args>(args)...);
+    static const int next_N = N + 1;
+    return std::forward<result>(std::get<N>(functions)(args...)) *
+      eval(int_<next_N>(), std::forward<Args>(args)...);
   }
 
   auto GetFunctions() const
   {
     return functions;
+  }
+
+  template <class T>
+  bool operator==(const T & t) const
+  {
+    return VariadicEqual(*this, t);
   }
 
 private:
@@ -47,5 +60,60 @@ private:
 
   DEF_FF_TEMPLATE(Multiplication)
 
+  template <class ... Functions>
+  std::ostream & operator<<(std::ostream & s,
+			    Multiplication<Functions...> m)
+  {
+    StreamVariadic("Multiplication", m, s);
+    return s;
+  }
+
+  template <class F>
+  auto Multiply(F f)
+  {
+    return f;
+  }
+
+  template <class F, class ... Fs>
+  auto Multiply(F f, Fs...fs)
+  {
+    return f * Multiply(fs...);
+  }
+
+  template <class ... Functions>
+  struct Simplification<
+    Multiplication<Functions...>,
+    typename std::enable_if<
+      (sizeof...(Functions) > 2)>::type>
+  {
+    typedef bool_<
+      has_abelian_arithmetic<
+	Multiplication<
+	  Functions...>>::value> Abelian;
+    typedef decltype(SimplifyV<Multiplication>
+		     (std::declval<std::tuple<Functions...>>(),
+		      Abelian{})) type;
+
+    static type Combine(Multiplication<Functions...> a)
+    {
+      return SimplifyV<Multiplication>(a.GetFunctions(),
+				       Abelian{});
+    }
+  };
+
+  template <class F, class ... Functions>
+  struct Simplification<
+    Multiplication<F, Multiplication<Functions...>>>
+  {
+    typedef Multiplication<F, Functions...> type;
+
+    static type Combine(Multiplication<F, 
+			Multiplication<Functions...>> t)
+    {
+      auto tu = t.GetFunctions();
+      return {std::tuple_cat(remove_element<1>(tu),
+			     std::get<1>(tu).GetFunctions())};
+    }
+  };
 }
 #endif
