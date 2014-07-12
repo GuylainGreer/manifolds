eeb671cd43f2c45d5d2607f4c3d88b4c92fed749
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 16:48:59 -0400

    Testing tuple-input-unpacking

--- test_full_function_defs.cpp
+++ test_full_function_defs.cpp
@@ -3,10 +3,26 @@
 #include "variables.hh"
 #include "trig.hh"
 
+struct ReturnTwo : manifolds::Function
+{
+  auto operator()() const
+  {
+    return std::make_tuple(1,2);
+  }
+};
+
 BOOST_AUTO_TEST_CASE(full_function_test)
 {
   using namespace manifolds;
-  auto f = Sin()(t);
+  auto f = Sin()(x);
 
   BOOST_CHECK_EQUAL(f(3,4,5), std::sin(3));
+
+  //Testing automatic tuple-unpacking
+  ReturnTwo rt;
+  auto one = x(rt);
+  auto two = y(rt);
+
+  BOOST_CHECK_EQUAL(one(), 1);
+  BOOST_CHECK_EQUAL(two(), 2);
 }
