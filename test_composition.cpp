d352494f9362cacd52dc242798f595c0a9c7d9dc
GuylainGreer <guylaingreer@gmail.com>
2014-07-12 16:46:56 -0400

    Added a few more tests, nothing major

--- test_composition.cpp
+++ test_composition.cpp
@@ -7,12 +7,12 @@
 BOOST_AUTO_TEST_CASE(composition_test)
 {
   using namespace manifolds;
-  Composition<Sin, decltype(z)> a;
-
+  Composition<Sin, decltype(t)> a;
+  static_assert(decltype(a)::stateless, "");
   BOOST_CHECK_EQUAL(a(1,2,3,4), std::sin(4));
   BOOST_CHECK_EQUAL(a(1,2,4,8,16), std::sin(8));
 
   Composition<Sin, Cos> b;
-  auto c = b(z);
+  auto c = b(t);
   BOOST_CHECK_EQUAL(c(1,2,3,4), std::sin(std::cos(4)));
 }
