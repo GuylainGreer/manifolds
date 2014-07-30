00a39bd28015b9e68266aec8fb8839d11de58a9c
GuylainGreer <guylaingreer@gmail.com>
2014-07-30 09:04:46 -0400

    Added optional simplification debug messages and IsVariadic template metafunction

--- simplify.hh
+++ simplify.hh
@@ -3,6 +3,7 @@
 
 #include "tuple_util.hh"
 #include <boost/type_traits/is_same.hpp>
+#include <iostream>
 
 namespace manifolds {
   //Default, don't do anything
@@ -17,9 +18,19 @@ namespace manifolds {
     }
   };
 
+  template <template<class...>class Variadic, class...>
+  struct IsVariadic : std::false_type{};
+
+  template <template<class...>class V, class ... Fs,
+	    class ... Fs2>
+  struct IsVariadic<V, V<Fs...>,Fs2...> : std::true_type{};
+
   template <template <class...> class Variadic,
 	    class ... Args1, class ... Args2>
-  struct Simplification<Variadic<Variadic<Args1...>, Args2...> >
+  struct Simplification<
+    Variadic<Variadic<Args1...>, Args2...>,
+    typename std::enable_if<
+      !IsVariadic<Variadic,Args2...>::value>::type>
   {
     typedef Variadic<Args1..., Args2...> type;
 
@@ -42,26 +53,38 @@ namespace manifolds {
     static type Combine(Variadic<Variadic<Args1...>,
 			Variadic<Args2...>> input)
     {
-      return type(std::tuple_cat(std::get<0>(input),
-				 std::get<1>(input)));
+      auto t1 =
+	std::get<0>(input.GetFunctions()).GetFunctions();
+      auto t2 =
+	std::get<1>(input.GetFunctions()).GetFunctions();
+      return type(std::tuple_cat(t1, t2));
     }
   };
 
   template <class F>
   auto Simplify(F f, F)
   {
+#ifdef PRINT_SIMPLIFIES
+    std::cout << "Done: " << f << std::endl;
+#endif
     return f;
   }
 
   template <class F, class G>
   auto Simplify(F f, G)
   {
+#ifdef PRINT_SIMPLIFIES
+    std::cout << "Simplifying: " << f << std::endl;
+#endif
     return Simplify(Simplification<F>::Combine(f), f);
   }
 
   template <class F>
   auto Simplify(F f)
   {
+#ifdef PRINT_SIMPLIFIES
+    std::cout << "Simplifying: " << f << std::endl;
+#endif
     return Simplify(Simplification<F>::Combine(f), f);
   }
 }
