29b7c360ca7e733709caba1abdd9e7dc07b87ce9
GuylainGreer <guylaingreer@gmail.com>
2014-07-30 09:06:25 -0400

    Added more tests

--- test_derivative.cpp
+++ test_derivative.cpp
@@ -9,4 +9,12 @@ BOOST_AUTO_TEST_CASE(derivative_test)
   auto d = Derivative(f);
   auto check = -2_c * x * Sin()(x * x);
   BOOST_CHECK_EQUAL(d(2), check(2));
+
+  D<2> d2;
+  auto ddf = d2(f);
+  auto ddf2 = Derivative(d);
+  BOOST_CHECK_EQUAL(ddf(1), ddf2(1));
+  std::cout << f << "\n\n"
+	    << Derivative(f) << "\n\n"
+	    << Derivative(d) << "\n\n";
 }
