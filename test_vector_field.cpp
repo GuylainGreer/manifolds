5f3ea8c87415e507d8b9b47c2f6fcc9ae3eefc12
GuylainGreer <guylaingreer@gmail.com>
2014-08-05 17:22:25 -0400

    Adding VectorField class and unit test

---
+++ test_vector_field.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+#include "vector_field.hh"
+#include "trig.hh"
+#include "polynomial.hh"
+
+BOOST_AUTO_TEST_CASE(vector_field_test)
+{
+  using namespace manifolds;
+  auto v1 = GetVectorField(1_c, 1_c);
+  auto f1 = v1(Sin()(x*y));
+  auto f1_check = Cos()(x * y) * (x + y);
+  BOOST_CHECK_EQUAL(f1(2, 1), f1_check(2, 1));
+  BOOST_CHECK_CLOSE(f1(4, 3), f1_check(4, 3), 1E-13);
+}
---
+++ vector_field.hh
@@ -0,0 +1,44 @@
+#ifndef MANIFOLDS_FUNCTIONS_VECTOR_FIELDS_HH
+#define MANIFOLDS_FUNCTIONS_VECTOR_FIELDS_HH
+
+#include <tuple>
+#include "operators.hh"
+#include "derivatives.hh"
+
+namespace manifolds
+{
+  template <class ... Functions>
+  struct VectorField
+  {
+    std::tuple<Functions...> functions;
+
+    VectorField(Functions ... functions):
+      functions(functions...){}
+    VectorField(std::tuple<Functions...> functions):
+      functions(functions){}
+
+    template <class F, std::size_t ... indices>
+    auto eval(F f, std::integer_sequence<
+	      std::size_t, indices...>) const
+    {
+      return Add((std::get<indices>(functions)*
+		  Derivative(f, Variable<indices>()))...);
+    }
+
+    template <class F,
+	      class = typename std::enable_if<
+		is_function<F>::value>::type>
+    auto operator()(F f) const
+    {
+      return eval(f, std::index_sequence_for<Functions...>());
+    }
+  };
+
+  template <class ... Functions>
+  auto GetVectorField(Functions ... functions)
+  {
+    return VectorField<Functions...>(functions...);
+  }
+}
+
+#endif
