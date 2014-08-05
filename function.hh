e521d4955b0a3b783221ea1b69cf4de0ba68e218
GuylainGreer <guylaingreer@gmail.com>
2014-08-05 17:27:07 -0400

    Added dimensionality metadata, removed MultiFunction class and moved template mate-functions to tmp_util.hh

--- function.hh
+++ function.hh
@@ -2,57 +2,21 @@
 #define MANIFOLDS_FUNCTION_INTERFACE_HH
 
 #include <utility>
+#include "tmp_utils.hh"
 
 namespace manifolds {
 
   //Tag
+  template <int INPUT_DIM, int OUTPUT_DIM>
   struct Function
   {
     static const bool stateless = false;
+    static const int input_dim = INPUT_DIM;
+    static const int output_dim = OUTPUT_DIM;
     //Means you can change the order of
     //Multiplications, ie A*B == B*A
     static const bool abelian_arithmetic = true;
   };
 
-  //Tag
-  struct MultiFunction : Function
-  {
-  };
-
-  template <class T>
-  struct is_stateless
-  {
-    static const bool value = T::stateless;
-    typedef std::integral_constant<
-      bool, value> type;
-  };
-
-  template <class T>
-  struct has_abelian_arithmetic
-  {
-    static const bool value = T::abelian_arithmetic;
-    typedef std::integral_constant<
-      bool, value> type;
-  };
-
-  template <class...Args>
-  struct and_;
-
-  template <class Arg, class ... Args>
-  struct and_<Arg,Args...> : std::conditional<
-    Arg::value, and_<Args...>, std::false_type>::type{};
-
-  template <class Arg>
-  struct and_<Arg> : std::conditional<
-    Arg::value, std::true_type, std::false_type>::type{};
-
-  template <>
-  struct and_<> : std::true_type{};
-
-  template <int N>
-  using int_ = std::integral_constant<int, N>;
-
-  template <bool B>
-  using bool_ = std::integral_constant<bool, B>;
 }
 #endif
