2d79ae214e886da46cde42b52085ee1eef299d8a
GuylainGreer <guylaingreer@gmail.com>
2014-08-06 07:29:08 -0400

    Adding test for one of the division simplifications and the IsVariadic meta-function

--- test_division.cpp
+++ test_division.cpp
@@ -1,6 +1,7 @@
 #include "division.hh"
 #include <boost/test/unit_test.hpp>
 #include "full_function_defs.hh"
+#include "operators.hh"
 #include "trig.hh"
 
 BOOST_AUTO_TEST_CASE(reciprocal_test)
@@ -9,4 +10,8 @@ BOOST_AUTO_TEST_CASE(reciprocal_test)
 
   Division<Sin, Cos> t;
   BOOST_CHECK_EQUAL(t(2), std::sin(2)/std::cos(2));
+  auto d1 = Sin() / Log();
+  auto d2 = Pow() / Tan();
+  static_assert(IsVariadic<Division,
+		decltype(d1*d2)>::value,"");
 }
