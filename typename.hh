ece368e54cf4b25ed136bd01e7a2f85e49f695ac
GuylainGreer <guylaingreer@gmail.com>
2014-08-06 07:33:44 -0400

    Adding filter to remove manifolds namespace, much cleaner now

--- typename.hh
+++ typename.hh
@@ -11,6 +11,13 @@ std::string get_cpp_name()
     char * name = abi::__cxa_demangle(typeid(T).name(),0,0,0);
     std::string real_name = name;
     free(name);
+    auto i = real_name.find("manifolds::");
+    while(i != std::string::npos)
+      {
+	real_name.erase(real_name.begin()+i,
+			real_name.begin()+i+11);
+	i = real_name.find("manifolds::");
+      }
     return real_name;
 }
 
