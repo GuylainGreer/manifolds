c6a341b553be095f5503f569f54a225ec12c4e5b
GuylainGreer <guylaingreer@gmail.com>
2014-08-06 07:31:02 -0400

    Testing more derivative capabilities

--- test_derivative.cpp
+++ test_derivative.cpp
@@ -5,16 +5,21 @@ BOOST_AUTO_TEST_CASE(derivative_test)
 {
   using namespace manifolds;
   BOOST_CHECK_EQUAL(Derivative(Sin()), Cos());
-  auto f = Cos()(x*x);
+  auto f = Cos()(x*x) * y;
   auto d = Derivative(f);
-  auto check = -2_c * x * Sin()(x * x);
-  BOOST_CHECK_EQUAL(d(2), check(2));
+  auto check = -2_c * x * Sin()(x * x) * y;
+  BOOST_CHECK_EQUAL(d(2, 1), check(2, 1));
 
   D<2> d2;
   auto ddf = d2(f);
   auto ddf2 = Derivative(d);
-  BOOST_CHECK_EQUAL(ddf(1), ddf2(1));
-  std::cout << f << "\n\n"
-	    << Derivative(f) << "\n\n"
-	    << Derivative(d) << "\n\n";
+  BOOST_CHECK_EQUAL(ddf(1, 1), ddf2(1, 1));
+  BOOST_CHECK_EQUAL(ddf(2, 1), ddf2(2, 1));
+  BOOST_CHECK_EQUAL(ddf(4, 1), ddf2(4, 1));
+  BOOST_CHECK_EQUAL(ddf(7, 1), ddf2(7, 1));
+  auto fd = FullDerivative(f);
+  auto fd_check =
+    GetFunctionMatrix(GetRow(d, Cos()(x*x)));
+  BOOST_CHECK_EQUAL(fd(5,6), fd_check(5,6));
+  BOOST_CHECK_EQUAL(fd(7,7), fd_check(7,7));
 }
