e05980cd11ffd6fff4045cdbc11ac65cf2a8d9fb
GuylainGreer <guylaingreer@gmail.com>
2014-08-06 07:28:11 -0400

    Adding is_function test

--- test_addition.cpp
+++ test_addition.cpp
@@ -2,6 +2,7 @@
 #include "addition.hh"
 #include "variables.hh"
 #include "full_function_defs.hh"
+#include "trig.hh"
 
 BOOST_AUTO_TEST_CASE(addition_test)
 {
@@ -10,4 +11,6 @@ BOOST_AUTO_TEST_CASE(addition_test)
 
   BOOST_CHECK_EQUAL(a(1,2,3,4), 4);
   BOOST_CHECK_EQUAL(a(1,2,4,8,16), 5);
+  auto l = Log()(x);
+  static_assert(is_function<decltype(l)>::value, "");
 }
