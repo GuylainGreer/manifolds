4d8fd129b139ea9ee1fb4961efe641ade9e0ee51
GuylainGreer <guylaingreer@gmail.com>
2014-08-15 07:04:14 -0400

    Improved composition of multi-dimensional functions

--- composition.hh
+++ composition.hh
@@ -6,8 +6,55 @@
 #include "equals.hh"
 #include "streaming.hh"
 #include "simplify_variadic.hh"
+#include "group.hh"
 
 namespace manifolds {
+
+template <int output_dim>
+struct ToTuple
+{
+  template <class X, std::size_t ... indices>
+  auto eval(X x, std::integer_sequence<
+	    std::size_t, indices...>,
+	    int_<1>) const
+  {
+    return x;
+  }
+
+  template <class X, std::size_t ... indices>
+  auto eval(X x, std::integer_sequence<
+	    std::size_t, indices...>,
+	    int_<2>) const
+  {
+    static const int iter_rows = X::num_rows > 1;
+    static const int iter_cols = X::num_cols > 1;
+    static_assert(iter_rows == 1 ||
+		  iter_cols == 1,
+		  "I'm not sure what's going on here");
+    return std::make_tuple(x.Coeff(indices * iter_rows,
+				   indices * iter_cols)...);
+  }
+
+  template <class X, std::size_t ... indices>
+  auto eval(X x, std::integer_sequence<
+	    std::size_t, indices...>,
+	    int_<4>) const
+  {
+    return std::make_tuple(std::get<indices>(x)...);
+  }
+
+  template <class X>
+  auto operator()(X x) const
+  {
+    static const int tag =
+      output_dim == 1 ?
+      1 :
+      (2 * (has_coeff_function<X>::value) +
+       4 * (is_gettable<X>::value));
+    return eval(x, std::make_index_sequence<output_dim>(),
+		int_<tag>());
+  }
+};
 
 template <class ... Functions>
 struct Composition :
@@ -32,30 +79,36 @@ struct Composition :
   auto operator()(Arg arg, Args ... args) const
   {
     static const int last = sizeof...(Functions)-1;
-    return eval(std::get<last>(functions)(arg, args...),
+    static const int next_ins =
+      nth<last-1, Functions...>::type::input_dim;
+    ToTuple<next_ins> t;
+    return eval(t(std::get<last>(functions)(arg, args...)),
 		int_<last-1>());
   }
 
   auto operator()() const
   {
     static const int last = sizeof...(Functions)-1;
-    return eval(std::get<last>(functions)(),
+    static const int next_ins =
+      nth<last-1, Functions...>::type::input_dim;
+    ToTuple<next_ins> t;
+    return eval(t(std::get<last>(functions)()),
 		int_<last-1>());
   }
 
   template <class Arg>
   auto eval(Arg arg, int_<0>) const
   {
-    typedef decltype(std::get<0>(functions)(arg)) result;
-    return std::forward<result>(std::get<0>(functions)(arg));
+    return std::get<0>(functions)(arg);
   }
 
   template <int N, class Arg>
   auto eval(Arg arg, int_<N>) const
   {
-    typedef decltype(std::get<N>(functions)(arg)) result;
-    return eval(std::forward<result>
-		(std::get<N>(functions)(arg)),
+    static const int next_ins =
+      nth<N-1, Functions...>::type::input_dim;
+    ToTuple<next_ins> t;
+    return eval(t(std::get<N>(functions)(arg)),
 		int_<N-1>());
   }
 
@@ -76,13 +129,23 @@ struct Composition :
 		    prepare<InnerFunc>>(*this, f));
   }
 
+  template <class ... InnerFuncs,
+	    class = typename std::enable_if<
+	      (sizeof...(InnerFuncs) > 1) &&
+	      and_<is_function<InnerFuncs>...>::value
+	      >::type>
+  auto operator()(InnerFuncs ... ifs) const
+  {
+    return (*this)(GetGroup(ifs...));
+  }
+
   auto GetFunctions() const
   {
     return functions;
   }
 
   void stream(std::ostream & s,
-			int_<sizeof...(Functions)>)
+	      int_<sizeof...(Functions)>)
   {
   }
 
@@ -124,9 +187,7 @@ private:
   }
 
   template <class ... Functions>
-  struct Simplification<
-    Composition<Functions...>,
-    typename std::enable_if<(sizeof...(Functions)>2)>::type>
+  struct Simplification<Composition<Functions...>,2>
   {
     typedef decltype(SimplifyV<Composition>
 		     (std::declval<std::tuple<Functions...>>(),
@@ -139,23 +200,6 @@ private:
 #endif
       return SimplifyV<Composition>(a.GetFunctions(),
 				    std::false_type{});
-    }
-  };
-
-  template <class F, class ... Functions>
-  struct Simplification<
-    Composition<F, Composition<Functions...>>>
-  {
-    typedef Composition<F, Functions...> type;
-
-    static type Combine(Composition<F, Composition<Functions...>> c)
-    {
-#ifdef PRINT_SIMPLIFIES
-      std::cout << "Flattening composition\n";
-#endif
-      auto t = c.GetFunctions();
-      return {std::tuple_cat(remove_element<1>(t),
-			     std::get<1>(t).GetFunctions())};
     }
   };
 
