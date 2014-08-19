bb9967dc5713a4b752169515aeffbf81093d3fac
guylaingreer <guylaingreer@gmail.com>
2014-08-19 08:34:59 -0400

    Adding real-life function that needs work

--- test_addition.cpp
+++ test_addition.cpp
@@ -4,6 +4,8 @@
 #include "full_function_defs.hh"
 #include "trig.hh"
 #include "polynomial.hh"
+#include "operators.hh"
+#include <iostream>
 
 BOOST_AUTO_TEST_CASE(addition_test)
 {
@@ -15,4 +17,23 @@ BOOST_AUTO_TEST_CASE(addition_test)
   auto l = Log()(x);
   static_assert(is_function<decltype(l)>::value, "");
   static_assert(!IsVariadic<Composition, Cos>::value, "");
+  Variable<0> b0;
+  Variable<1> b1;
+  Variable<2> b2;
+  Variable<4> b3;
+  Variable<5> m0;
+  Variable<6> m1;
+  Variable<7> m2;
+  Variable<8> m3;
+  auto c12i = t;
+  auto c12r = b0 + m0 * t;
+  auto c33 = b1 + m1 * t;
+  auto c22 = b2 + m2 * t;
+  auto c11 = b3 + m3 * t;
+  auto f =
+      4_c * c22 * c22 * c11 * c33 -
+      8_c * c22 * (c11 + c33) * c11 * c33 +
+      (c11 + c33) * (c11 + c33) * c11 * c33 -
+      (c12r * c12r + c12i * c12i) * (c11 + c33) * (c11 * c33);
+  std::cout << "\n\n" << f << "\n\n";
 }
