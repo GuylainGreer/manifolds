8a7b75db85bfac8a902ba2a09baa8d7a9a1cd679
guylaingreer <guylaingreer@gmail.com>
2014-09-25 08:27:27 -0400

    Moved to functions directory

--- boost_test.cpp
+++
@@ -1,4 +0,0 @@
-#define BOOST_TEST_MAIN
-#include <boost/test/included/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(Test){}
--- test_complex.cpp
+++
@@ -1,7 +0,0 @@
-#include <boost/test/unit_test.hpp>
-#include "complex.hh"
-
-BOOST_AUTO_TEST_CASE(complex_test)
-{
-  
-}
