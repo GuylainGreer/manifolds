19bc9c77fe7d3798cc9280325a2db072c7c8a5f6
guylaingreer <guylaingreer@gmail.com>
2014-10-03 07:36:58 -0400

    Fixing up equality comparison

--- functions/multiplication.hh
+++ functions/multiplication.hh
@@ -7,68 +7,67 @@
 
 namespace manifolds {
 
-template <class ... Functions>
-struct MultiplicationImpl :
-    Function<
-  list<int_<5>, typename Functions::indices...>,
-  max<Functions::input_dim...>::value,
-  max<Functions::output_dim...>::value>
+    template <class ... Functions>
+    struct MultiplicationImpl :
+        Function<
+        list<int_<5>, typename Functions::indices...>,
+        max<Functions::input_dim...>::value,
+        max<Functions::output_dim...>::value>
+    {
+        static const bool stateless =
+            and_<is_stateless<Functions>...>::value;
+        static const ComplexOutputBehaviour complex_output =
+            VariadicComplexOutput<Functions...>::value;
+
+        MultiplicationImpl(){}
+        MultiplicationImpl(const Functions & ... functions):
+            functions(functions...){}
+        MultiplicationImpl(const tuple<Functions...> & f):
+            functions(f){}
+
+        template <class ... Args>
+        auto operator()(Args ... args) const
+        {
+            return eval(int_<0>(), args...);
+        }
+
+        template <class ... Args>
+        auto eval(int_<sizeof...(Functions)-1>, Args ... args) const
+        {
+            return get<sizeof...(Functions)-1>
+                (functions)(args...);
+        }
+
+        template <int N, class ... Args>
+        auto eval(int_<N>, Args ... args) const
+        {
+            typedef decltype(get<N>(functions)(args...)) result;
+            return std::forward<result>(get<N>(functions)(args...)) *
+                eval(int_<N + 1>(), std::forward<Args>(args)...);
+        }
+
+        auto GetFunctions() const
+        {
+            return functions;
+        }
+
+    private:
+        tuple<Functions...> functions;
+    };
+
+DEF_FF_TEMPLATE(Multiplication)
+
+template <class ... Fs>
+Multiplication<Fs...> MultiplyRaw(Fs ... fs)
 {
-  static const bool stateless =
-    and_<is_stateless<Functions>...>::value;
-  static const ComplexOutputBehaviour complex_output =
-    VariadicComplexOutput<Functions...>::value;
-
-  MultiplicationImpl(){}
-  MultiplicationImpl(const Functions & ... functions):
-    functions(functions...){}
-  MultiplicationImpl(const tuple<Functions...> & f):
-    functions(f){}
-
-  template <class ... Args>
-  auto operator()(Args ... args) const
-  {
-    return eval(int_<0>(), args...);
-  }
-
-  template <class ... Args>
-  auto eval(int_<sizeof...(Functions)-1>, Args ... args) const
-  {
-    return get<sizeof...(Functions)-1>
-      (functions)(args...);
-  }
-
-  template <int N, class ... Args>
-  auto eval(int_<N>, Args ... args) const
-  {
-    typedef decltype(get<N>(functions)(args...)) result;
-    return std::forward<result>(get<N>(functions)(args...)) *
-      eval(int_<N + 1>(), std::forward<Args>(args)...);
-  }
-
-  auto GetFunctions() const
-  {
-    return functions;
-  }
-
-  template <class T>
-  bool operator==(const T & t) const
-  {
-    return VariadicEqual(*this, t);
-  }
-
-private:
-  tuple<Functions...> functions;
-};
-
-  DEF_FF_TEMPLATE(Multiplication)
-
-  template <class ... Fs>
-  Multiplication<Fs...> MultiplyRaw(Fs ... fs)
-  {
     return {fs...};
-  }
+}
 
+template <class ... Args, class T>
+bool operator==(Multiplication<Args...> m, T t)
+{
+    return VariadicEqual(m, t);
+}
 }
 
 #endif
