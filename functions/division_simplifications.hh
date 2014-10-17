c1af70d57a02c0f8ad105b0336b09d5c5fe1ec4a
guylaingreer <guylaingreer@gmail.com>
2014-10-17 17:32:16 -0400

    adding simplification for division of divisions

--- functions/division_simplifications.hh
+++ functions/division_simplifications.hh
@@ -7,103 +7,121 @@
 
 namespace manifolds {
 
-  template <class N1, class N2, class D1, class D2>
-  struct Simplification<
-    Multiplication<Division<N1,D1>,
-		   Division<N2,D2>>, /*mult_div_div*/0>
-  {
-    typedef Division<
-      Multiplication<N1,N2>,
-      Multiplication<D1,D2>> type;
-
-    static type Combine(Multiplication<Division<N1,D1>,
-			Division<N2,D2>> a)
+    template <class N1, class N2, class D1, class D2>
+    struct Simplification<
+        Multiplication<Division<N1,D1>,
+                       Division<N2,D2>>, /*mult_div_div*/0>
     {
-      SIMPLIFY_INFO("Simplifying multiplication of division "
-		    "by division\n");
-      Multiplication<N1,N2>
-	mn(get<0>(a.GetFunctions()).GetNumerator(),
-	   get<1>(a.GetFunctions()).GetNumerator());
+        typedef Division<
+            Multiplication<N1,N2>,
+            Multiplication<D1,D2>> type;
 
-      Multiplication<D1,D2>
-	md(get<0>(a.GetFunctions()).GetDenominator(),
-	   get<1>(a.GetFunctions()).GetDenominator());
+        static type Combine(Multiplication<Division<N1,D1>,
+                            Division<N2,D2>> a)
+        {
+            SIMPLIFY_INFO("Simplifying multiplication of division "
+                          "by division\n");
+            Multiplication<N1,N2>
+                mn(get<0>(a.GetFunctions()).GetNumerator(),
+                   get<1>(a.GetFunctions()).GetNumerator());
 
-      return {mn, md};
-    }
-  };
+            Multiplication<D1,D2>
+                md(get<0>(a.GetFunctions()).GetDenominator(),
+                   get<1>(a.GetFunctions()).GetDenominator());
 
-  template <class N, class D, class F>
-  struct Simplification<
-    Multiplication<Division<N,D>,F>, /*mult_div_f*/1>
-  {
-    typedef Division<
-      Multiplication<N,F>, D> type;
+            return {mn, md};
+        }
+    };
 
-    static type Combine(Multiplication<Division<N,D>,F> a)
+    template <class N, class D, class F>
+    struct Simplification<
+        Multiplication<Division<N,D>,F>, /*mult_div_f*/1>
     {
-      SIMPLIFY_INFO("Simplifying multiplication of division "
-		    "by function\n");
-      Multiplication<N,F>
-	m(get<0>(a.GetFunctions()).GetNumerator(),
-	  get<1>(a.GetFunctions()));
-      return {m, get<0>(a.GetFunctions()).GetDenominator()};
-    }
-  };
+        typedef Division<
+            Multiplication<N,F>, D> type;
 
-  template <class N, class D, class F>
-  struct Simplification<Division<Division<N,D>,F>,
-			/*div_div_f*/0>
-  {
-    typedef Division<N, Multiplication<D,F>> type;
+        static type Combine(Multiplication<Division<N,D>,F> a)
+        {
+            SIMPLIFY_INFO("Simplifying multiplication of division "
+                          "by function\n");
+            Multiplication<N,F>
+                m(get<0>(a.GetFunctions()).GetNumerator(),
+                  get<1>(a.GetFunctions()));
+            return {m, get<0>(a.GetFunctions()).GetDenominator()};
+        }
+    };
 
-    static type Combine(Division<Division<N,D>,F> a)
+    template <class N1, class D1, class N2, class D2>
+    struct Simplification<
+        Division<Division<N1,D1>,Division<N2,D2> >,
+                 /*div_div_div*/0>
     {
-      SIMPLIFY_INFO("Simplifying division of "
-		    "division by function\n");
-      return {a.GetNumerator().GetNumerator(),
-	  Multiplication<D,F>(a.GetNumerator().GetDenominator(),
-			      a.GetDenominator())};
-    }
-  };
+        static auto Combine(Division<
+                                Division<N1,D1>,
+                                Division<N2,D2> > d)
+        {
+            auto n = d.GetNumerator();
+            auto de = d.GetDenominator();
+            return DivideRaw(MultiplyRaw(n.GetNumerator(),
+                                         de.GetDenominator()),
+                             MultiplyRaw(n.GetDenominator(),
+                                         de.GetNumerator()));
+        }
+    };
 
-  template <class N, class D, class F>
-  struct Simplification<Division<F, Division<N,D>>,
-			/*div_f_div*/1>
-  {
+    template <class N, class D, class F>
+    struct Simplification<Division<Division<N,D>,F>,
+                          /*div_div_f*/1>
+    {
+        typedef Division<N, Multiplication<D,F>> type;
+
+        static type Combine(Division<Division<N,D>,F> a)
+        {
+            SIMPLIFY_INFO("Simplifying division of "
+                          "division by function\n");
+            return {a.GetNumerator().GetNumerator(),
+                    Multiplication<D,F>(a.GetNumerator().GetDenominator(),
+                                        a.GetDenominator())};
+        }
+    };
+
+template <class N, class D, class F>
+struct Simplification<Division<F, Division<N,D>>,
+                      /*div_f_div*/2>
+{
     typedef Division<Multiplication<D,F>, N> type;
 
     static type Combine(Division<Division<N,D>,F> a)
     {
-      SIMPLIFY_INFO("Simplifying division of "
-		    "division by function\n");
-      return {Multiplication<D,F>(a.GetNumerator().
-				  GetDenominator(),
-				  a.GetDenominator()),
-	  a.GetNumerator().GetNumerator()};
+        SIMPLIFY_INFO("Simplifying division of "
+                      "division by function\n");
+        return {Multiplication<D,F>(a.GetNumerator().
+                                    GetDenominator(),
+                                    a.GetDenominator()),
+                a.GetNumerator().GetNumerator()};
     }
-  };
+};
 
-  template <class N, class D, class ... Fs>
-  struct Simplification<
+template <class N, class D, class ... Fs>
+struct Simplification<
     Composition<Division<N,D>, Fs...>, /*com_div_fs*/0>
-  {
+{
     typedef Division<
-      Composition<N, Fs...>,
-      Composition<D, Fs...>> type;
+        Composition<N, Fs...>,
+        Composition<D, Fs...>> type;
 
     static type Combine(Composition<Division<N,D>, Fs...> c)
     {
-      auto t = c.GetFunctions();
-      auto ds = get<0>(t).GetFunctions();
-      auto fs = remove_element<0>(t);
-      auto n = Composition<N, Fs...>
-	(tuple_cat(remove_element<1>(ds), fs));
-      auto d = Composition<D, Fs...>
-	(tuple_cat(remove_element<0>(ds), fs));
-      return {n, d};
+        auto t = c.GetFunctions();
+        auto ds = get<0>(t).GetFunctions();
+        auto fs = remove_element<0>(t);
+        auto n = Composition<N, Fs...>
+            (tuple_cat(remove_element<1>(ds), fs));
+        auto d = Composition<D, Fs...>
+            (tuple_cat(remove_element<0>(ds), fs));
+        return {n, d};
     }
-  };
+};
 }
 
 #endif
