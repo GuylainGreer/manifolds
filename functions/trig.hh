0d3e0db94af4aa3136d14010c46bc06c0b5153ad
guylaingreer <guylaingreer@gmail.com>
2014-10-17 17:33:28 -0400

    fixing pow function a little bit

--- functions/trig.hh
+++ functions/trig.hh
@@ -14,7 +14,7 @@
         static const ComplexOutputBehaviour complex_output =	\
             SameAsInput;                                        \
         template <class T>                                      \
-        auto eval(T t) const                                    \
+            auto eval(T t) const                                \
         {                                                       \
             using std::funcname;                                \
             return funcname(t);					\
@@ -30,40 +30,41 @@
 
 namespace manifolds
 {
-  STD_FUNCTION(Sin, sin, 8)
-  STD_FUNCTION(Cos, cos, 9)
-  STD_FUNCTION(Tan, tan, 10)
-  STD_FUNCTION(Log, log, 11)
-  STD_FUNCTION(Sinh, sinh, 12)
-  STD_FUNCTION(Cosh, cosh, 13)
-  STD_FUNCTION(Tanh, tanh, 14)
-  STD_FUNCTION(ASin, asin, 15)
-  STD_FUNCTION(ACos, acos, 16)
-  STD_FUNCTION(ATan, atan, 17)
-  STD_FUNCTION(ASinh, asinh, 18)
-  STD_FUNCTION(ACosh, acosh, 19)
-  STD_FUNCTION(ATanh, atanh, 20)
-  STD_FUNCTION(Exp, exp, 21)
-  STD_FUNCTION(Sqrt, sqrt, 22)
+    STD_FUNCTION(Sin, sin, 8)
+    STD_FUNCTION(Cos, cos, 9)
+    STD_FUNCTION(Tan, tan, 10)
+    STD_FUNCTION(Log, log, 11)
+    STD_FUNCTION(Sinh, sinh, 12)
+    STD_FUNCTION(Cosh, cosh, 13)
+    STD_FUNCTION(Tanh, tanh, 14)
+    STD_FUNCTION(ASin, asin, 15)
+    STD_FUNCTION(ACos, acos, 16)
+    STD_FUNCTION(ATan, atan, 17)
+    STD_FUNCTION(ASinh, asinh, 18)
+    STD_FUNCTION(ACosh, acosh, 19)
+    STD_FUNCTION(ATanh, atanh, 20)
+    STD_FUNCTION(Exp, exp, 21)
+    STD_FUNCTION(Sqrt, sqrt, 22)
 #undef STD_FUNCTION
 
-  struct Pow : Function<int_<23>, 2,1>,
-      FunctionCommon<Pow>
-  {
-    static const bool stateless = true;
-    static const ComplexOutputBehaviour complex_output =
-      SameAsInput;
-    template <class T, class U>
-    auto eval(T t, U u) const
+    struct Pow : Function<int_<23>, 2, 1>,
+        FunctionCommon<Pow>
     {
-      return std::pow(t, u);
-    }
-  };
+        static const bool stateless = true;
+        static const ComplexOutputBehaviour complex_output =
+            SameAsInput;
+        template <class T, class U>
+        auto eval(T t, U u) const
+        {
+            using std::pow;
+            return pow(t, u);
+        }
+    };
 
-  inline std::ostream & operator<<(std::ostream & s, Pow p)
-  {
-    return s << "pow";
-  }
+    inline std::ostream & operator<<(std::ostream & s, Pow p)
+    {
+        return s << "pow";
+    }
 }
 
 #endif
