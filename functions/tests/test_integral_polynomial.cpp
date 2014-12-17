83e2f91027d52cb870a2f9dad20272a84e4cb58f
guylaingreer <guylaingreer@gmail.com>
2014-12-16 20:38:10 -0500

    Replacing manual, multiple-value-testing with call to PointWiseEqual

--- functions/tests/test_integral_polynomial.cpp
+++ functions/tests/test_integral_polynomial.cpp
@@ -4,6 +4,7 @@
 #include "functions/streaming.hh"
 #include "functions/operators.hh"
 #include "functions/polynomial_simplifications.hh"
+#include "pointwise_equal.hh"
 
 BOOST_AUTO_TEST_CASE(integral_polynomial_test)
 {
@@ -14,14 +15,9 @@ BOOST_AUTO_TEST_CASE(integral_polynomial_test)
   auto p2 = GetPolynomial(3,4);
   //std::cout << (ip1 + ip1 * ip2) << std::endl;
   //std::cout << (p1 + p1 * p2) << std::endl;
-  BOOST_CHECK_EQUAL(ip1(1), p1(1));
-  BOOST_CHECK_EQUAL(ip1(2), p1(2));
-  BOOST_CHECK_EQUAL(ip2(4), p2(4));
-  BOOST_CHECK_EQUAL(ip2(8), p2(8));
+  PointwiseEqual(ip1, p1, 2);
+  PointwiseEqual(ip2, p2, 2);
   auto ip3 = ip1 + ip1 * ip2;
   auto p3 = p1 + p1 * p2;
-  BOOST_CHECK_EQUAL(ip3(1), p3(1));
-  BOOST_CHECK_EQUAL(ip3(2), p3(2));
-  BOOST_CHECK_EQUAL(ip3(4), p3(4));
-  BOOST_CHECK_EQUAL(ip3(8), p3(8));
+  PointwiseEqual(ip3, p3, 4);
 }
