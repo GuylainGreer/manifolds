8b4d276b45cc50c6901419bc3772e4faec7e0d9c
guylaingreer <guylaingreer@gmail.com>
2015-01-29 08:43:58 -0500

    Adding check that negative 0 polynomial gives zero

--- functions/tests/test_integral_polynomial.cpp
+++ functions/tests/test_integral_polynomial.cpp
@@ -12,11 +12,11 @@ BOOST_AUTO_TEST_CASE(integral_polynomial_test) {
   auto ip2 = GetIPolynomial<3, 4>();
   auto p1 = GetPolynomial(1, -2);
   auto p2 = GetPolynomial(3, 4);
-  // std::cout << (ip1 + ip1 * ip2) << std::endl;
-  // std::cout << (p1 + p1 * p2) << std::endl;
   PointwiseEqual(ip1, p1, 2);
   PointwiseEqual(ip2, p2, 2);
   auto ip3 = ip1 + ip1 * ip2;
   auto p3 = p1 + p1 * p2;
   PointwiseEqual(ip3, p3, 4);
+
+  BOOST_CHECK_EQUAL(-GetIPolynomial<0>(), zero);
 }
