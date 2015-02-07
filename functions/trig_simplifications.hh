d3f73531d5c6df88b0abe883b2bb39b91277def9
guylaingreer <guylaingreer@gmail.com>
2015-02-07 13:45:10 -0500

    Adding new simplifications

--- functions/trig_simplifications.hh
+++ functions/trig_simplifications.hh
@@ -14,10 +14,9 @@
 namespace manifolds {
 
 template <>
-struct Simplification<Multiplication<Cos, Cos>,
+struct Simplification<Composition<IntegralPolynomial<0,0,1>, Cos>,
                       /*mult_cos_cos*/ 0> {
-  typedef Multiplication<Cos, Cos> in_type;
-  static auto Combine(in_type) {
+  static auto Combine(Composition<IntegralPolynomial<0,0,1>, Cos>) {
     SIMPLIFY_INFO("Simplifying cos**2\n");
     return ComposeRaw(GetIPolynomial<1, 0, -1>(), Sin());
   }
@@ -54,12 +53,14 @@ struct Simplification<
 
 template <> struct Simplification<Composition<Sin, ACos>, 0> {
   static auto Combine(Composition<Sin, ACos>) {
+      SIMPLIFY_INFO("Simplifying Sin(ACos)\n");
     return Sqrt()(GetIPolynomial<1, 0, -1>());
   }
 };
 
 template <> struct Simplification<Composition<Sin, ATan>, 0> {
   static auto Combine(Composition<Sin, ATan>) {
+      SIMPLIFY_INFO("Simplifying Sin(ATan)\n");
     auto hypo = Sqrt()(GetIPolynomial<1, 0, 1>());
     return DivideRaw(GetIPolynomial<0, 1>(), hypo);
   }
@@ -67,12 +68,14 @@ template <> struct Simplification<Composition<Sin, ATan>, 0> {
 
 template <> struct Simplification<Composition<Cos, ASin>, 0> {
   static auto Combine(Composition<Cos, ASin>) {
+      SIMPLIFY_INFO("Simplifying Cos(ASin)\n");
     return Sqrt()(GetIPolynomial<1, 0, -1>());
   }
 };
 
 template <> struct Simplification<Composition<Cos, ATan>, 0> {
   static auto Combine(Composition<Cos, ATan>) {
+      SIMPLIFY_INFO("Simplifying Cos(ATan)\n");
     auto hypo = Sqrt()(GetIPolynomial<1, 0, 1>());
     return DivideRaw(GetIPolynomial<1>(), hypo);
   }
@@ -80,12 +83,14 @@ template <> struct Simplification<Composition<Cos, ATan>, 0> {
 
 template <> struct Simplification<Composition<Tan, ASin>, 0> {
   static auto Combine(Composition<Tan, ASin>) {
+      SIMPLIFY_INFO("Simplifying Tan(ASin)\n");
     return DivideRaw(GetIPolynomial<0, 1>(), GetIPolynomial<1, 0, -1>());
   }
 };
 
 template <> struct Simplification<Composition<Tan, ACos>, 0> {
   static auto Combine(Composition<Tan, ACos>) {
+      SIMPLIFY_INFO("Simplifying Tan(ACos)\n");
     return DivideRaw(GetIPolynomial<1>(), GetIPolynomial<1, 0, -1>());
   }
 };
@@ -109,6 +114,7 @@ struct Simplification<
         last<ic<coeffs>...>::type::value < 0)>::type> {
 
   static auto Combine(Composition<Sin, IntegralPolynomial<coeffs...> >) {
+      SIMPLIFY_INFO("Simplifying Sin(-n*x^m)\n");
     return Negative(Sin()(Negative(IntegralPolynomial<coeffs...>())));
   }
 };
@@ -125,11 +131,13 @@ struct Simplification<
 
   template <std::size_t... indices>
   static auto Combine(std::integer_sequence<std::size_t, indices...>) {
-    return std::make_pair(GetIPolynomial<(indices, 0)..., left>(),
-                          GetIPolynomial<(indices, 0)..., right>());
+      SIMPLIFY_INFO("Simplifying Sin(n*x^m)\n");
+    return std::make_pair(GetIPolynomial<(indices* 0)..., left>(),
+                          GetIPolynomial<(indices* 0)..., right>());
   }
 
   static auto Combine(Composition<Sin, IntegralPolynomial<coeffs...> >) {
+      SIMPLIFY_INFO("Simplifying Sin(n*x^m)\n");
     auto subs = Combine(std::make_index_sequence<sizeof...(coeffs) - 1>());
     auto left = subs.first;
     auto right = subs.second;
@@ -145,6 +153,7 @@ struct Simplification<
         last<ic<coeffs>...>::type::value < 0)>::type> {
 
   static auto Combine(Composition<Cos, IntegralPolynomial<coeffs...> >) {
+      SIMPLIFY_INFO("Simplifying Cos(-n*x^m)\n");
     return Cos()(Negative(IntegralPolynomial<coeffs...>()));
   }
 };
@@ -161,11 +170,12 @@ struct Simplification<
 
   template <std::size_t... indices>
   static auto Combine(std::integer_sequence<std::size_t, indices...>) {
-    return std::make_pair(GetIPolynomial<(indices, 0)..., left>(),
-                          GetIPolynomial<(indices, 0)..., right>());
+    return std::make_pair(GetIPolynomial<(indices* 0)..., left>(),
+                          GetIPolynomial<(indices* 0)..., right>());
   }
 
   static auto Combine(Composition<Cos, IntegralPolynomial<coeffs...> >) {
+      SIMPLIFY_INFO("Simplifying Cos(n*x^m)\n");
     auto subs = Combine(std::make_index_sequence<sizeof...(coeffs) - 1>());
     auto left = subs.first;
     auto right = subs.second;
@@ -173,6 +183,91 @@ struct Simplification<
                   MultiplyRaw(Sin()(left), Sin()(right)));
   }
 };
+
+    template <IPInt_t ... coeffs>
+    struct SplitPoly
+    {
+        template <int i>
+        using ith = typename nth<i, std::integral_constant<IPInt_t, coeffs>...>::type;
+
+        template <int i, int DUMMY = 0>
+        struct NonZero
+        {
+            static const int index = sizeof...(coeffs) - 1 - i;
+            static const int value =
+                ith<index>::value == 0 ?
+                NonZero<i-1, 0>::value : i;
+        };
+
+        template <int DUMMY>
+        struct NonZero<-1, DUMMY>
+        {
+            static const int value = -1;
+        };
+
+        static const int first = NonZero<sizeof...(coeffs)-1>::value;
+        static_assert(first != -1, "");
+
+        template <class>
+        struct LeftPoly;
+
+        template <std::size_t ... indices>
+        struct LeftPoly<std::integer_sequence<std::size_t, indices...> >
+        {
+            typedef IntegralPolynomial<ith<indices>::value...> type;
+        };
+
+        template <class>
+        struct RightPoly;
+
+        template <std::size_t ... indices>
+        struct RightPoly<std::integer_sequence<std::size_t, indices...> >
+        {
+            typedef std::tuple<std::integral_constant<IPInt_t, coeffs>...> tup;
+            typedef std::integral_constant<IPInt_t, 0> zero;
+            typedef decltype(replace_element<first>(tup(), zero())) tup_z;
+            typedef IntegralPolynomial<std::tuple_element<indices, tup_z>::type::value...> type;
+        };
+
+        typedef boost::mpl::pair<
+            typename LeftPoly<std::make_index_sequence<first+1> >::type,
+            typename RightPoly<std::make_index_sequence<sizeof...(coeffs)> >::type
+            > type;
+    };
+
+    template <IPInt_t ... coeffs>
+    struct Simplification<
+        Composition<Sin, IntegralPolynomial<coeffs...> >, 0,
+        typename std::enable_if<!is_all_but_last_0<coeffs...>::value>::type>
+    {
+        typedef typename SplitPoly<coeffs...>::type Splits;
+        typedef typename Splits::first Left;
+        typedef typename Splits::second Right;
+
+        static auto Combine(Composition<Sin, IntegralPolynomial<coeffs...> >)
+        {
+            SIMPLIFY_INFO("Simplifying Sin(poly + poly)\n");
+            return AddRaw(MultiplyRaw(Sin{}(Left{}), Cos{}(Right{})),
+                          MultiplyRaw(Cos{}(Left{}), Sin{}(Right{})));
+        }
+    };
+
+    template <IPInt_t ... coeffs>
+    struct Simplification<
+        Composition<Cos, IntegralPolynomial<coeffs...> >, 0,
+        typename std::enable_if<!is_all_but_last_0<coeffs...>::value>::type>
+    {
+        typedef typename SplitPoly<coeffs...>::type Splits;
+        typedef typename Splits::first Left;
+        typedef typename Splits::second Right;
+
+        static auto Combine(Composition<Cos, IntegralPolynomial<coeffs...> >)
+        {
+            SIMPLIFY_INFO("Simplifying Cos(poly + poly)\n");
+            return SubRaw(MultiplyRaw(Cos{}(Left{}), Cos{}(Right{})),
+                          MultiplyRaw(Sin{}(Left{}), Sin{}(Right{})));
+        }
+    };
 }
 
 #endif
