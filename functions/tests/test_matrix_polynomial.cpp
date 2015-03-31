519c2be970842edd2215dae2af1ff4b600a575c3
guylaingreer <guylaingreer@gmail.com>
2015-03-31 08:08:10 -0400

    Adding new unit test testing that polynomial class works with matrices

---
+++ functions/tests/test_matrix_polynomial.cpp
@@ -0,0 +1,22 @@
+#include "functions/integral_polynomial.hh"
+#include "functions/polynomial.hh"
+#include "functions/operators.hh"
+#include <boost/test/unit_test.hpp>
+#include <complex>
+#include "functions/trig.hh"
+#include "functions/all_simplifications.hh"
+#include "functions/matrix.hh"
+
+BOOST_AUTO_TEST_CASE(matrix_polynomial_test) {
+  using namespace manifolds;
+
+  auto m = GetMatrix<2, 2>(1.0, 0.0, 0.0, 1.0);
+  auto p = GetPolynomial(m, m * 2);
+  BOOST_CHECK_EQUAL(p(2), 5 * m);
+
+  BOOST_CHECK_EQUAL(p(m), 3 * m);
+
+  auto p2 = p * p + 2_c * p;
+  auto p2_check = GetPolynomial(3 * m, 8 * m, 4 * m);
+  BOOST_CHECK(p2 == p2_check);
+}
